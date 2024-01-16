82de7653f7f5e81c48a6a7bf27cc0fc2d296c32d
GedeonTS <gedeontshobohwa1@gmail.com>
2024-01-16 17:31:01 +0200

    Remove unused class

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Api;
 
 use App\Models\Order;
 use Illuminate\Http\Request;
-use Illuminate\Support\Carbon;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 
